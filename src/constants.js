98cd6d83d29a2ca1a3b4d7320229b3302789fb03
Max Nowack <max@unsou.de>
2016-12-06 22:59:25 +0100

    implement higher level thermostat class
    in order to do a useful code separation and make it easier to
    understand the application flow

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const TargetHeatingCoolingState = {
+  OFF: 'TargetHeatingCoolingState.OFF',
+  HEAT: 'TargetHeatingCoolingState.HEAT',
+  AUTO: 'TargetHeatingCoolingState.AUTO',
+  COOL: 'TargetHeatingCoolingState.COOL',
+}
+
+export const CurrentHeatingCoolingState = {
+  OFF: 'CurrentHeatingCoolingState.OFF',
+  HEAT: 'CurrentHeatingCoolingState.HEAT',
+}
