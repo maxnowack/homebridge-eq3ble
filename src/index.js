5d35f7fb9e731a39953478811c3298ae2ca67214
Max Nowack <max@unsou.de>
2016-12-06 20:58:07 +0100

    rename factory

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import thermostat from './thermostat'
+import thermostatFactory from './factory'
 
 module.exports = function register(homebridge) {
   const Service = homebridge.hap.Service
   const Characteristic = homebridge.hap.Characteristic
 
-  homebridge.registerAccessory('homebridge-eq3ble', 'EQ3-Thermostat', thermostat({
+  homebridge.registerAccessory('homebridge-eq3ble', 'EQ3-Thermostat', thermostatFactory({
     Service,
     Characteristic,
   }))
