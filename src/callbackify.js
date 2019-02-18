ae48fc576df46c71dc543fcba4eee12b12bf110a
Max Nowack <max@unsou.de>
2019-02-18 14:17:56 +0100

    Use callbackify function from homebridge-tesla https://github.com/nfarina/homebridge-tesla/blob/247b5584cc77b79e2aea6b48e265a60e27ad9a80/src/util/callbackify.ts resolves #23

--- src/callbackify.js
+++ src/callbackify.js
@@ -1,9 +1,26 @@
-export default function callbackify(fn) {
-  return (callback, ...args) => {
-    fn(...args).then((...results) => {
-      if (callback) callback(null, ...results)
-    }, (err) => {
-      if (callback) callback(err)
+// copied from https://github.com/nfarina/homebridge-tesla/blob/247b5584cc77b79e2aea6b48e265a60e27ad9a80/src/util/callbackify.ts
+export default function callbackify(func) {
+  return (...args) => {
+    const onlyArgs = []
+    let maybeCallback = null
+
+    args.forEach((arg) => {
+      if (typeof maybeCallback === 'function') return
+      if (typeof arg === 'function') {
+        maybeCallback = arg
+        return
+      }
+      onlyArgs.push(arg)
     })
+
+    if (!maybeCallback) {
+      throw new Error('Missing callback parameter!')
+    }
+
+    const callback = maybeCallback
+
+    func(...onlyArgs)
+      .then(data => callback(null, data))
+      .catch(err => callback(err))
   }
 }
